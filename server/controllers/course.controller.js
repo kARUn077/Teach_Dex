06a370e113b186418b68c38dc90eebb32d974d3d
Navneet Maurya <navneetmaurya108@gmail.com>
2025-05-29 04:55:35 +0530

    logic for creating the lecture

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -172,3 +172,36 @@ export const getCourseById = async (req,res) => {
         })
     }
 }
+
+export const createLecture = async (req,res) => {
+    try {
+        const {lectureTitle} = req.body;
+        const {courseId} = req.params;
+
+        if(!lectureTitle || !courseId){
+            return res.status(400).json({
+                message:"Lecture title is required"
+            })
+        };
+
+        // create lecture
+        const lecture = await Lecture.create({lectureTitle});
+
+        const course = await Course.findById(courseId);
+        if(course){
+            course.lectures.push(lecture._id);
+            await course.save();
+        }
+
+        return res.status(201).json({
+            lecture,
+            message:"Lecture created successfully."
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to create lecture"
+        })
+    }
+}
