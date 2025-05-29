d38b9e8e2f2e51d75c4fb569bc6eb48a87362478
Navneet Maurya <navneetmaurya108@gmail.com>
2025-05-30 04:12:03 +0530

    logic for getting lecture by Id

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -295,3 +295,23 @@ export const removeLecture = async (req,res) => {
         })
     }
 }
+
+export const getLectureById = async (req,res) => {
+    try {
+        const {lectureId} = req.params;
+        const lecture = await Lecture.findById(lectureId);
+        if(!lecture){
+            return res.status(404).json({
+                message:"Lecture not found!"
+            });
+        }
+        return res.status(200).json({
+            lecture
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to get lecture by id"
+        })
+    }
+}
