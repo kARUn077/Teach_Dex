ddbd65bd40701e9dd05745f1bc9f523332fa4ab1
Navneet Maurya <navneetmaurya108@gmail.com>
2025-05-30 04:08:46 +0530

    logic for updating the lecture

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -226,3 +226,41 @@ export const getCourseLecture = async (req,res) => {
         })
     }
 }
+
+export const editLecture = async (req,res) => {
+    try {
+        const {lectureTitle, videoInfo, isPreviewFree} = req.body;
+        
+        const {courseId, lectureId} = req.params;
+        const lecture = await Lecture.findById(lectureId);
+        if(!lecture){
+            return res.status(404).json({
+                message:"Lecture not found!"
+            })
+        }
+
+        // update lecture
+        if(lectureTitle) lecture.lectureTitle = lectureTitle;
+        if(videoInfo?.videoUrl) lecture.videoUrl = videoInfo.videoUrl;
+        if(videoInfo?.publicId) lecture.publicId = videoInfo.publicId;
+        lecture.isPreviewFree = isPreviewFree;
+
+        await lecture.save();
+
+        // Ensure the course still has the lecture id if it was not aleardy added;
+        const course = await Course.findById(courseId);
+        if(course && !course.lectures.includes(lecture._id)){
+            course.lectures.push(lecture._id);
+            await course.save();
+        };
+        return res.status(200).json({
+            lecture,
+            message:"Lecture updated successfully."
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:"Failed to edit lectures"
+        })
+    }
+}
