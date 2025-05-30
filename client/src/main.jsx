6547a50fc7777d63f79691efb31f9b6d87ba05dc
karun217 <karunpoddar0@gmail.com>
2025-05-31 00:03:43 +0530

    add main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,12 @@
 import App from "./App.jsx";
-
+import { Provider } from "react-redux";
+import { appStore } from "./app/store";
+import { Toaster } from "./components/ui/sonner";
+import { useLoadUserQuery } from "./features/api/authApi";
+import LoadingSpinner from "./components/LoadingSpinner";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
 const Custom = ({ children }) => {
   const { isLoading } = useLoadUserQuery();
