7152acb878e7693a5dcce6e9d57829fec939afa9
karun217 <karunpoddar0@gmail.com>
2025-05-31 00:10:53 +0530

    add login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -11,3 +11,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import {
+  useLoginUserMutation,
+  useRegisterUserMutation,
+} from "@/features/api/authApi";
+import { Loader2 } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
