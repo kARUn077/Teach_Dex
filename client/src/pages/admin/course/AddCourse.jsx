21f6100da187e5767d00fcc3d944a4832b424f81
karun217 <karunpoddar0@gmail.com>
2025-06-07 23:58:28 +0530

    add add course

---
+++ client/src/pages/admin/course/AddCourse.jsx
@@ -0,0 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useCreateCourseMutation } from "@/features/api/courseApi";
+import { Loader2 } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+const AddCourse = () => {
+  const [courseTitle, setCourseTitle] = useState("");
+  const [category, setCategory] = useState("");
+
+  const [createCourse, { data, isLoading, error, isSuccess }] =
+    useCreateCourseMutation();
+
+  const navigate = useNavigate();
+
+  const getSelectedCategory = (value) => {
+    setCategory(value);
+  };
+
+  const createCourseHandler = async () => {
+    await createCourse({ courseTitle, category });
+  };
+
+  // for displaying toast
+  useEffect(()=>{
+    if(isSuccess){
+        toast.success(data?.message || "Course created.");
+        navigate("/admin/course");
+    }
+  },[isSuccess, error])
