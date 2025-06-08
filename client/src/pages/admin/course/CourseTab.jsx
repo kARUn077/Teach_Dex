6f40b163538f78f5319f39985b0a6a4f0ea172e1
karun217 <karunpoddar0@gmail.com>
2025-06-08 23:29:18 +0530

    add course tab

--- client/src/pages/admin/course/CourseTab.jsx
+++ client/src/pages/admin/course/CourseTab.jsx
@@ -27,3 +27,37 @@ import { Loader2 } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "sonner";
+
+const CourseTab = () => {
+  
+  const [input, setInput] = useState({
+    courseTitle: "",
+    subTitle: "",
+    description: "",
+    category: "",
+    courseLevel: "",
+    coursePrice: "",
+    courseThumbnail: "",
+  });
+
+  const params = useParams();
+  const courseId = params.courseId;
+  const { data: courseByIdData, isLoading: courseByIdLoading , refetch} =
+    useGetCourseByIdQuery(courseId);
+
+    const [publishCourse, {}] = usePublishCourseMutation();
+ 
+  useEffect(() => {
+    if (courseByIdData?.course) { 
+        const course = courseByIdData?.course;
+      setInput({
+        courseTitle: course.courseTitle,
+        subTitle: course.subTitle,
+        description: course.description,
+        category: course.category,
+        courseLevel: course.courseLevel,
+        coursePrice: course.coursePrice,
+        courseThumbnail: "",
+      });
+    }
+  }, [courseByIdData]);
