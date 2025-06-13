4e4e5a4fb1e3583011448d8ab36c656abc33b23a
karun217 <karunpoddar0@gmail.com>
2025-06-13 23:18:55 +0530

    add course

---
+++ client/src/pages/student/CourseDetail.jsx
@@ -0,0 +1,35 @@
+import BuyCourseButton from "@/components/BuyCourseButton";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { useGetCourseDetailWithStatusQuery } from "@/features/api/purchaseApi";
+import { BadgeInfo, Lock, PlayCircle } from "lucide-react";
+import React from "react";
+import ReactPlayer from "react-player";
+import { useNavigate, useParams } from "react-router-dom";
+
+const CourseDetail = () => {
+  const params = useParams();
+  const courseId = params.courseId;
+  const navigate = useNavigate();
+  const { data, isLoading, isError } =
+    useGetCourseDetailWithStatusQuery(courseId);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h>Failed to load course details</h>;
+
+  const { course, purchased } = data;
+  console.log(purchased);
+
+  const handleContinueCourse = () => {
+    if(purchased){
+      navigate(`/course-progress/${courseId}`)
+    }
+  }
