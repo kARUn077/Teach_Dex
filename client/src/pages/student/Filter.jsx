ac6648393ce6e2a92942194b52d085ab4d6370bb
karun217 <karunpoddar0@gmail.com>
2025-06-22 23:40:53 +0530

    correct erros ,as galat naam ho gya

--- client/src/pages/student/Filter.jsx
+++ client/src/pages/student/Filter.jsx
@@ -13,18 +13,19 @@ import { Separator } from "@/components/ui/separator";
 import React, { useState } from "react";
 
 const categories = [
-  { id: "nextjs", label: "Next JS" },
-  { id: "data science", label: "Data Science" },
-  { id: "frontend development", label: "Frontend Development" },
-  { id: "fullstack development", label: "Fullstack Development" },
-  { id: "mern stack development", label: "MERN Stack Development" },
-  { id: "backend development", label: "Backend Development" },
-  { id: "javascript", label: "Javascript" },
-  { id: "python", label: "Python" },
-  { id: "docker", label: "Docker" },
-  { id: "mongodb", label: "MongoDB" },
-  { id: "html", label: "HTML" },
+  { id: "Next JS", label: "Next JS" },
+  { id: "Data Science", label: "Data Science" },
+  { id: "Frontend Development", label: "Frontend Development" },
+  { id: "Fullstack Development", label: "Fullstack Development" },
+  { id: "MERN Stack Development", label: "MERN Stack Development" },
+  { id: "Backend Development", label: "Backend Development" },
+  { id: "Javascript", label: "Javascript" },
+  { id: "Python", label: "Python" },
+  { id: "Docker", label: "Docker" },
+  { id: "MongoDB", label: "MongoDB" },
+  { id: "HTML", label: "HTML" },
 ];
+
 
 const Filter = ({ handleFilterChange }) => {
   const [selectedCategories, setSelectedCategories] = useState([]);
@@ -36,15 +37,15 @@ const Filter = ({ handleFilterChange }) => {
         ? prevCategories.filter((id) => id !== categoryId)
         : [...prevCategories, categoryId];
 
-        handleFilterChange(newCategories, sortByPrice);
-        return newCategories;
+      handleFilterChange(newCategories, sortByPrice);
+      return newCategories;
     });
   };
 
   const selectByPriceHandler = (selectedValue) => {
     setSortByPrice(selectedValue);
     handleFilterChange(selectedCategories, selectedValue);
-  }
+  };
   return (
     <div className="w-full md:w-[20%]">
       <div className="flex items-center justify-between">
@@ -66,14 +67,13 @@ const Filter = ({ handleFilterChange }) => {
       <div>
         <h1 className="font-semibold mb-2">CATEGORY</h1>
         {categories.map((category) => (
-          <div className="flex items-center space-x-2 my-2">
+          <div key={category.id} className="flex items-center space-x-2 my-2">
             <Checkbox
               id={category.id}
+              checked={selectedCategories.includes(category.id)}
               onCheckedChange={() => handleCategoryChange(category.id)}
             />
-            <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-              {category.label}
-            </Label>
+            <Label htmlFor={category.id}>{category.label}</Label>
           </div>
         ))}
       </div>
