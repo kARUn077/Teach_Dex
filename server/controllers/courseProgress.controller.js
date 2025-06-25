efa6931c8b57e331bfbebeea562c9759d52aae1b
utkarshup32 <utkarshdwivedi1879@gmail.com>
2025-06-26 01:21:19 +0530

    added course progress

---
+++ server/controllers/courseProgress.controller.js
@@ -0,0 +1,45 @@
+import { CourseProgress } from "../models/courseProgress.js";
+import { Course } from "../models/course.model.js";
+
+export const getCourseProgress = async (req, res) => {
+  try {
+    const { courseId } = req.params;
+    const userId = req.id;
+
+    // step-1 fetch the user course progress
+    let courseProgress = await CourseProgress.findOne({
+      courseId,
+      userId,
+    }).populate("courseId");
+
+    const courseDetails = await Course.findById(courseId).populate("lectures");
+
+    if (!courseDetails) {
+      return res.status(404).json({
+        message: "Course not found",
+      });
+    }
+
+    // Step-2 If no progress found, return course details with an empty progress
+    if (!courseProgress) {
+      return res.status(200).json({
+        data: {
+          courseDetails,
+          progress: [],
+          completed: false,
+        },
+      });
+    }
+
+    // Step-3 Return the user's course progress alog with course details
+    return res.status(200).json({
+      data: {
+        courseDetails,
+        progress: courseProgress.lectureProgress,
+        completed: courseProgress.completed,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
