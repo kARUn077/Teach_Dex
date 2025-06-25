fb05d3150e7a8a70c00255e241bd39b0260dd6de
karun217 <karunpoddar0@gmail.com>
2025-06-25 23:59:30 +0530

    add course purchase

---
+++ server/controllers/coursePurchase.controller.js
@@ -0,0 +1,66 @@
+import Stripe from "stripe";
+import { Course } from "../models/course.model.js";
+import { CoursePurchase } from "../models/coursePurchase.model.js";
+import { Lecture } from "../models/lecture.model.js";
+import { User } from "../models/user.model.js";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+// Create Stripe Checkout Session
+export const createCheckoutSession = async (req, res) => {
+  try {
+    const userId = req.id;
+    const { courseId } = req.body;
+
+    const course = await Course.findById(courseId);
+    if (!course) return res.status(404).json({ message: "Course not found!" });
+
+    const newPurchase = new CoursePurchase({
+      courseId,
+      userId,
+      amount: course.coursePrice,
+      status: "pending",
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "inr",
+            product_data: {
+              name: course.courseTitle,
+              images: [course.courseThumbnail],
+            },
+            unit_amount: course.coursePrice * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: `http://localhost:5173/course-progress/${courseId}`,
+      cancel_url: `http://localhost:5173/course-detail/${courseId}`,
+      metadata: {
+        courseId,
+        userId,
+      },
+      shipping_address_collection: {
+        allowed_countries: ["IN"],
+      },
+    });
+
+    if (!session.url) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Stripe session creation failed" });
+    }
+
+    newPurchase.paymentId = session.id;
+    await newPurchase.save();
+
+    res.status(200).json({ success: true, url: session.url });
+  } catch (error) {
+    console.error("Stripe session error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
